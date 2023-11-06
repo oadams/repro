735aea62d898fc9be2ed829cfbdf43e1057b2768
Oliver Adams <oliver.adams@gmail.com>
2023-11-06 13:36:02 +1100

    Introduced a new Stage ADT to represent stages in our DAGs.

--- src/Parse.hs
+++ src/Parse.hs
@@ -8,24 +8,29 @@ import Data.YAML
 import qualified Data.Text as T
 import qualified Data.ByteString.Lazy as BL
 
-data Stage = Stage
-    { name :: T.Text
-    , cmd  :: T.Text
-    , deps :: [T.Text]
-    , outs :: [T.Text]
+data YamlStage = YamlStage
+    { yamlName :: T.Text
+    , yamlCmd  :: T.Text
+    , yamlDeps :: [T.Text]
+    , yamlOuts :: [T.Text]
     } deriving Show
 
-instance FromYAML Stage where
-   parseYAML = withMap "Stage" $ \m -> Stage
-       <$> m .: "name"
-       <*> m .: "cmd"
-       <*> m .:? "deps" .!= []
-       <*> m .:? "outs" .!= []
+instance FromYAML YamlStage where
+   parseYAML = withMap "YamlStage" $ \m -> YamlStage
+       <$> m .: "yamlName"
+       <*> m .: "yamlCmd"
+       <*> m .:? "yamlDeps" .!= []
+       <*> m .:? "yamlOuts" .!= []
+
+data Stage = Stage { name :: T.Text
+                   , cmd  :: T.Text
+                   , deps :: [Stage]
+                   } deriving Show
 
 someFunc :: IO ()
 someFunc = do
     contents <- BL.readFile "repro.yaml"
-    let decoded = decode1 contents :: Either (Pos,String) [Stage]
+    let decoded = decode1 contents :: Either (Pos,String) [YamlStage]
     case decoded of
         Left err -> putStrLn $ "An error occurred: " ++ show err
         Right stages -> do
