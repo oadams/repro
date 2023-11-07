ef4d96fe60f63c3cdb4ec57aec46a33356e1fa29
Oliver Adams <oliver.adams@gmail.com>
2023-11-07 17:07:29 +1100

    A couple more basic tests.

--- test/Spec.hs
+++ test/Spec.hs
@@ -23,6 +23,22 @@ yamldag1 = [
     ,YamlStage {yamlName="b", yamlCmd="echo b > b.txt", yamlDeps=["a.txt"], yamlOuts=["b.txt"]}    
     ]
 
+yamlempty :: [YamlStage]
+yamlempty = []
+
+yamlUnconnected :: [YamlStage]
+yamlUnconnected = [
+     YamlStage {yamlName="a", yamlCmd="echo a > a.txt", yamlDeps=[], yamlOuts=["a.txt"]}    
+    ,YamlStage {yamlName="b", yamlCmd="echo b > b.txt", yamlDeps=[], yamlOuts=["b.txt"]}    
+    ]
+
+yamlUnconnectedWithCycle :: [YamlStage]
+yamlUnconnectedWithCycle = [
+     YamlStage {yamlName="a", yamlCmd="echo a > a.txt", yamlDeps=[], yamlOuts=["a.txt"]}    
+    ,YamlStage {yamlName="b", yamlCmd="echo b > b.txt", yamlDeps=["c.txt"], yamlOuts=["b.txt"]}    
+    ,YamlStage {yamlName="c", yamlCmd="echo c > c.txt", yamlDeps=["b.txt"], yamlOuts=["c.txt"]}    
+    ]
+
 unitTests :: TestTree
 unitTests = testGroup "Unit tests"
   [ testCase "Detect a simple cycle" $
@@ -32,4 +48,16 @@ unitTests = testGroup "Unit tests"
   , testCase "Simple DAG is not a cycle" $
     let graph = constructDAG yamldag1
     in isCyclic graph @?= False
+
+  , testCase "Empty YAML is not a cycle" $
+    let graph = constructDAG yamlempty
+    in isCyclic graph @?= False
+
+  , testCase "Unconnected YAML" $
+    let graph = constructDAG yamlUnconnected
+    in isCyclic graph @?= False
+
+  , testCase "Unconnected YAML with a cycle" $
+    let graph = constructDAG yamlUnconnectedWithCycle
+    in isCyclic graph @?= True
   ]
