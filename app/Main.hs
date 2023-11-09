9d75de6951ebbbaf2533d943af11073a731f5818
Oliver Adams <oliver.adams@gmail.com>
2023-11-10 07:21:52 +1100

    Added a function to call git to get the hash of a file.

--- app/Main.hs
+++ app/Main.hs
@@ -1,25 +1,31 @@
 module Main (main) where
 
-import Parse ( readDag , Dag(..) , Stage(..))
+import Parse ( readDag , Dag(..) , Stage(..) )
 
 import Data.Maybe (catMaybes)
 import qualified Data.Map as Map
-import Data.Text (Text, unpack)
+import Data.Text (Text)
+import qualified Data.Text as T
 import qualified Data.Set as Set
+import System.Directory (doesFileExist)
 import System.Exit ( ExitCode(ExitFailure, ExitSuccess) )
 import System.Process
 
--- I should construct the graph from YAML in Parse.hs but then in this module
--- define isCyclic and then just call isCyclic when topologicalSort is called.
--- That way topologicalSort never has to assume isCyclic has already been
--- called. An alternative option would be to define both a Graph type and a Dag
--- type, with some sort of confirmCyclic :: Graph -> Maybe Dag and
--- topologicalSort :: Dag -> [Stage], but that is probably not as good as
--- topologicalSort :: Dag -> Maybe [Stage] Or perhaps better still: Leave
--- isCyclic in Parse.hs with constructDag, and leave the Dag type as is, but
--- make constructDag return a Maybe Dag or an Either String Dag and have
--- constructDag call isCyclic. That way, any Dag value is actually already
--- confirmed to be a Dag and doesn't rely on us checking it separately.
+
+gitHash :: FilePath -> IO Text
+gitHash path = do
+    gitOutput <- readProcess "git" ["hash-object", path] ""
+    return ((T.strip . T.pack) gitOutput)
+
+-- Need a function that takes in some dependency specified in the dag and:
+-- (a) determines if it is a file
+-- (b) if so computes its hash
+-- (c) if it is a directory then it must handle the recursive application to all
+-- the files in that director
+--hashDep :: FilePath -> IO String
+--hashDep path
+--    | doesFileExist path = gitHash path
+
 
 -- For a topological sort:
 -- Find the nodes that don't have any dependencies
@@ -58,7 +64,7 @@ getOrderedCommands (Dag mapDag) orderedStages =
 runCommands :: [Text] -> IO()
 runCommands [] = return ()
 runCommands (cmdtext:rest) = do
-    let cmd = unpack cmdtext
+    let cmd = T.unpack cmdtext
     putStrLn $ cmd
     exitCode <- system $ cmd
     case exitCode of 
